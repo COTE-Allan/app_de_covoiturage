71851cd62add643d6fd9cbac5d4ba4d44f0c614e
Augustin Petre <apetre.pro@gmail.com>
2021-05-12 11:52:33 +0200

    header et footer temporaire fini

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="fixed-bottom">
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <span class="boldify">A propos de nous</span><br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At quis risus sed vulputate odio ut. Tellus mauris a diam maecenas. Aenean euismod elementum nisi quis eleifend. Eget est lorem ipsum dolor sit amet. Arcu odio ut sem nulla pharetra. Vulputate dignissim suspendisse in est ante in nibh mauris. Urna nec tincidunt praesent semper feugiat nibh sed pulvinar proin.
+            </div>
+            <div class="col">
+                <span class="boldify">Contacte</span><br>
+                johndoe@gmail.com
+            </div>
+        </div>
+        <div class="row">
+            <span class="mx-auto">&copy; Copyright 2021 - 2021 covoite.fr</span>
+        </div>
+    </div>
+
+</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,56 @@
+<?php
+
+$loggedIn = false;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">
+                    <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+                    Covoite
+                </a>
+                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button> -->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Créer un trajet</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Chercher un trajet</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Conactez nous</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Infos</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <?php if($loggedIn == true){ ?>
+                <a href=""><img src="" width="30" height="30" class="d-inline-block align-top" alt=""></a>
+            <?php }else{ ?>
+                <button class="btn btn-outline-success my-2 my-sm-0">Connexion</button>
+            <?php } ?>
+        </nav>
+    </header>

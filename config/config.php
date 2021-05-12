f66e9f08b38c18c84efdd532a9948804ce30c973
Tom Delaunay <tdelaunay.lamanu@gmail.com>
2021-05-12 09:43:46 +0200

    Initialisation

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<!-- ---------------------------------------------
+    Fichier de Configuration
+---------------------------------------------- -->
+
+<?php
+// Informations des Pages
+$pages = [
+    ['index.php', 'Base Développement'],
+];
+// Informations Connexion BDD
+define('BDD_NAME', 'carpooling');
+define('BDD_USER', 'root');
+define('BDD_PWD',  '');
+define('BDD_HOST', 'localhost');
+?>
---
+++ functions.php
@@ -0,0 +1,26 @@
+<!-- ---------------------------------------------
+    Fichier de Fonctions
+---------------------------------------------- -->
+
+<?php
+// Fonctions à placer ici
+function bdd_connect()
+{
+    try {
+        $options = [
+            // Permet à PDO de lever des exceptions en cas d'erreur SQL
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+        // data source name
+        $dsn = 'mysql:host=' . BDD_HOST . ';dbname=' . BDD_NAME . ';charset=utf8';
+        // instance de la base de données (pdo)
+        $bdd_instance = new PDO($dsn, BDD_USER, BDD_PWD, $options);
+        // printf('Connexion Base De Données - Active');
+        return $bdd_instance;
+    } catch (PDOException $ex) {
+        printf('Connexion Base De Données - Erreur code "%s"', $ex->getCode());
+        // arrêter l'exécution du script
+        // die();
+    }
+}
+?>
